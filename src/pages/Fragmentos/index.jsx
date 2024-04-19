b19eb87038212e87b64b85af259a01d2b018b7ab
Jean Carlos Agostini <agostinijean@hotmail.com>
2024-04-19 14:59:25 -0300

    Fixing images

--- src/pages/Fragmentos/index.jsx
+++ src/pages/Fragmentos/index.jsx
@@ -10,31 +10,31 @@ const Fragmentos = () => {
       <div className='md:flex  md:flex-wrap md:flex-row md:gap-10 md:pt-10 '>
         <Personagem
           nome='ANDRIUS'
-          imagem='src/assets/personagens/Andrius/andrius_select.png'
+          imagem='src/assets/personagens/Andrius/select.png'
         />
         <Personagem
           nome='KHAMZAT'
-          imagem='src/assets/personagens/Khamzat/khamzat_select.png'
+          imagem='src/assets/personagens/Khamzat/select.png'
         />
         <Personagem
           nome='MIZO'
-          imagem='src/assets/personagens/Mizo/mizo_select.png'
+          imagem='src/assets/personagens/Mizo/select.png'
         />
         <Personagem
           nome='KAIOH'
-          imagem='src/assets/personagens/Kaioh/kaioh_select.png'
+          imagem='src/assets/personagens/Kaioh/select.png'
         />
         <Personagem
           nome='NIVEK'
-          imagem='src/assets/personagens/Nivek/nivek_select.jpg'
+          imagem='src/assets/personagens/Nivek/select.jpg'
         />
         <Personagem
           nome='QALLINNA'
-          imagem='src/assets/personagens/Qallinna/qalinna_select.png'
+          imagem='src/assets/personagens/Qallinna/select.png'
         />
         <Personagem
           nome='RHODEN'
-          imagem='src/assets/personagens/Rhoden/rhoden_select.png'
+          imagem='src/assets/personagens/Rhoden/select.png'
         />
       </div>
     </div>
