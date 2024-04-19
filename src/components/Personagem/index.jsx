576d3d4df877d55bb718f931f1b44ef3b4b4c875
Jean Carlos Agostini <agostinijean@hotmail.com>
2024-04-19 15:04:31 -0300

    Fixing images

--- src/components/Personagem/index.jsx
+++ src/components/Personagem/index.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 
-const Personagem = ({nome, imagem}) => {
+const Personagem = ({ nome, imagem }) => {
   return (
     <>
       <div className='w-[60vw] flex justify-center flex-col items-center pt-5 pb-3 md:w-[15vw] md:flex'>
-        <img
-          src={imagem}
-          className='rounded-full border-2 border-[#C89B3C]'
-        />
+        <img src={imagem} className='rounded-full border-2 border-[#C89B3C]' />
         <div className='pt-2 text-[#C89B3C] text-xl'>{nome}</div>
       </div>
     </>
--- src/pages/Fragmentos/index.jsx
+++ src/pages/Fragmentos/index.jsx
@@ -7,34 +7,34 @@ const Fragmentos = () => {
       <div>
         <span className='text-[#C89B3C] text-3xl'>PERSONAGENS</span>
       </div>
-      <div className='md:flex  md:flex-wrap md:flex-row md:gap-10 md:pt-10 '>
+      <div className='md:flex  md:flex-wrap md:flex-row md:gap-10 md:pt-10 md:justify-center '>
         <Personagem
           nome='ANDRIUS'
-          imagem='src/assets/personagens/Andrius/select.png'
+          imagem='https://raw.githubusercontent.com/Init-soga/RPG/main/src/assets/personagens/Andrius/select.png'
         />
         <Personagem
           nome='KHAMZAT'
-          imagem='src/assets/personagens/Khamzat/select.png'
+          imagem='https://raw.githubusercontent.com/Init-soga/RPG/main/src/assets/personagens/Khamzat/select.png'
         />
         <Personagem
           nome='MIZO'
-          imagem='src/assets/personagens/Mizo/select.png'
+          imagem='https://raw.githubusercontent.com/Init-soga/RPG/main/src/assets/personagens/Mizo/select.png'
         />
         <Personagem
           nome='KAIOH'
-          imagem='src/assets/personagens/Kaioh/select.png'
+          imagem='https://raw.githubusercontent.com/Init-soga/RPG/main/src/assets/personagens/Kaioh/select.png'
         />
         <Personagem
           nome='NIVEK'
-          imagem='src/assets/personagens/Nivek/select.jpg'
+          imagem='https://raw.githubusercontent.com/Init-soga/RPG/main/src/assets/personagens/Nivek/select.jpg'
         />
         <Personagem
           nome='QALLINNA'
-          imagem='src/assets/personagens/Qallinna/select.png'
+          imagem='https://raw.githubusercontent.com/Init-soga/RPG/main/src/assets/personagens/Qallinna/select.png'
         />
         <Personagem
           nome='RHODEN'
-          imagem='src/assets/personagens/Rhoden/select.png'
+          imagem='https://raw.githubusercontent.com/Init-soga/RPG/main/src/assets/personagens/Rhoden/select.png'
         />
       </div>
     </div>
