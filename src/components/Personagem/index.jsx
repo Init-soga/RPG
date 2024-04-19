e81c63b28bd2b465afe84930619306712ee66014
Jean Carlos Agostini <agostinijean@hotmail.com>
2024-04-19 14:40:36 -0300

    New images, deploying

--- src/components/Personagem/index.jsx
+++ src/components/Personagem/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Personagem = ({nome, imagem}) => {
   return (
     <>
-      <div className='w-[60vw] flex justify-center flex-col items-center pt-5'>
+      <div className='w-[60vw] flex justify-center flex-col items-center pt-5 pb-3 md:w-[15vw] md:flex'>
         <img
           src={imagem}
           className='rounded-full border-2 border-[#C89B3C]'
--- src/pages/Fragmentos/index.jsx
+++ src/pages/Fragmentos/index.jsx
@@ -3,14 +3,39 @@ import Personagem from '../../components/Personagem'
 
 const Fragmentos = () => {
   return (
-    <div className='flex justify-center items-center flex-col pt-5 text-'>
+    <div className='flex justify-center items-center flex-col pt-5'>
       <div>
-        <span className='text-[#C89B3C] text-3xl'>Personagens</span>
+        <span className='text-[#C89B3C] text-3xl'>PERSONAGENS</span>
       </div>
-      <div>
-        <Personagem nome="ANDRIUS" imagem="/src/assets/personagens/andrius.png"/>
-        <Personagem nome="KHAMZAT" imagem="/src/assets/personagens/khamzat.png"/>
-        <Personagem nome="MIZO" imagem="/src/assets/personagens/mizo.png"/>
+      <div className='md:flex  md:flex-wrap md:flex-row md:gap-10 md:pt-10 '>
+        <Personagem
+          nome='ANDRIUS'
+          imagem='/src/assets/personagens/Andrius/andrius_select.png'
+        />
+        <Personagem
+          nome='KHAMZAT'
+          imagem='/src/assets/personagens/Khamzat/khamzat_select.png'
+        />
+        <Personagem
+          nome='MIZO'
+          imagem='/src/assets/personagens/Mizo/mizo_select.png'
+        />
+        <Personagem
+          nome='KAIOH'
+          imagem='/src/assets/personagens/Kaioh/kaioh_select.png'
+        />
+        <Personagem
+          nome='NIVEK'
+          imagem='/src/assets/personagens/Nivek/nivek_select.jpg'
+        />
+        <Personagem
+          nome='QALLINNA'
+          imagem='/src/assets/personagens/Qallinna/qalinna_select.png'
+        />
+        <Personagem
+          nome='RHODEN'
+          imagem='/src/assets/personagens/Rhoden/rhoden_select.png'
+        />
       </div>
     </div>
   )
