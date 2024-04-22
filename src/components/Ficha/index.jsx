f1ffb834a6c636b6d036e9367a6be4898f536a75
Jean Carlos Agostini <agostinijean@hotmail.com>
2024-04-22 20:12:15 -0300

    Advancing on char pages

--- src/components/Ficha/index.jsx
+++ src/components/Ficha/index.jsx
@@ -3,20 +3,27 @@ import React from 'react'
 const Ficha = () => {
   return (
     <>
-      <div className="w-screen h-[30vh] border-2 border-[#C89B3C] bg-[url('https://raw.githubusercontent.com/Init-soga/RPG/main/src/assets/personagens/Khamzat/splash.png')] bg-cover bg-center"></div>
-      <div>
-        <span className='flex justify-center pt-4 text-3xl text-[#C89B3C]'>
-          Khamzat
+      <div className="w-screen h-[30vh] mb-4 md:h-[40vh] border-2 border-[#C89B3C] bg-[url('https://raw.githubusercontent.com/Init-soga/RPG/main/src/assets/personagens/Khamzat/splash.png')] bg-cover bg-center"></div>
+      <div className='border-[#C89B3C] border-2'>
+        <span className='flex justify-center text-3xl pt-2 text-[#C89B3C]'>
+          KHAMZAT
         </span>
-        <span className='p-5 text-center text-[#C89B3C] flex justify-center'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sint
-          ratione ex aliquid dolorem, consequatur earum quisquam! Iure ea sit
-          minus ipsam dolorum officia praesentium. Eaque consectetur odio
-          eligendi aspernatur necessitatibus consequuntur libero expedita,
-          similique mollitia at iusto quibusdam nam odit ipsam et cum voluptate
-          repudiandae dolor quas ratione cumque?
+        <span className='flex justify-center text-xl text-[#C89B3C]'>
+          O FILHO DE NOXUS
         </span>
+        <div className='flex justify-center'>
+          <div className="bg-[url('')]"></div>
+          <div></div>
+        </div>
       </div>
+      <span className='p-5 text-center text-[#C89B3C] flex justify-center'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga sint
+        ratione ex aliquid dolorem, consequatur earum quisquam! Iure ea sit
+        minus ipsam dolorum officia praesentium. Eaque consectetur odio eligendi
+        aspernatur necessitatibus consequuntur libero expedita, similique
+        mollitia at iusto quibusdam nam odit ipsam et cum voluptate repudiandae
+        dolor quas ratione cumque?
+      </span>
     </>
   )
 }
