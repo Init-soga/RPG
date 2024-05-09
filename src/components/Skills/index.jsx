27e94286dc429562b4ecb599af65037e03ef4b6d
Jean Carlos Agostini <agostinijean@hotmail.com>
2024-05-09 20:40:07 -0300

    Setting back button and thumbs

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import ImageGallery from 'react-image-gallery'
 import 'react-image-gallery/styles/css/image-gallery.css'
+import { useNavigate } from 'react-router-dom'
 
 const Skills = ({ charData }) => {
   const carouselImages = charData.skills
 
+  const navigate = useNavigate()
+
+  const handleBack = () => {
+    navigate(`/personagens`)
+  }
+
   return (
     <div>
-      <div className='flex justify-center h-[10vh] pt-[3vh] text-xl text-[#C89B3C]'>
+      <div className='flex justify-center py-3 items-center font-bold text-xl text-[#C89B3C]'>
         Habilidades/Itens de {charData.name}
       </div>
-      <div className='h-full'>
+      <div className='w-[100%] pb-3'>
+        <img
+          src='https://raw.githubusercontent.com/Init-soga/RPG/main/src/assets/visuals/divider.png'
+          alt='Separator'
+        />
+      </div>
+      <div className='h-full px-5 pb-5'>
         <ImageGallery
           items={carouselImages}
           showNav={false}
@@ -20,6 +33,14 @@ const Skills = ({ charData }) => {
           infinite={false}
           showBullets={false}
         ></ImageGallery>
+      </div>
+      <div className='flex justify-center pb-5'>
+        <button
+          className='border-2 px-5 py-2 border-[#C89B3C] rounded-md text-[#C89B3C]'
+          onClick={handleBack}
+        >
+          Voltar
+        </button>
       </div>
     </div>
   )
